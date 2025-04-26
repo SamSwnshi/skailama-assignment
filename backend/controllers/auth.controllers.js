ebbfc0498e76b174694d63ddd3ecae8c43ce9893
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-04-26 12:59:54 +0530

    auth controller

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,98 @@
+import User from "../models/auth.models";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please provide Email & Password",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "User Not Found!",
+      });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({
+        message: "Invalid Credentials!",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+      }, //payload
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+      token,
+      message: "Login Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User is already Existing",
+      });
+    }
+
+    const hashpassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      email,
+      password: hashpassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: newUser._id,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({ message: "Logged Out Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
