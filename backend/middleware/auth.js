fbb2c3c53265e89b4561b8f9f3a3adf59aacef56
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-04-26 11:45:43 +0530

    auth middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+const auth = async(req,res,next)=>{
+    const token = req.header("Authorization")?.split(" ")
+    if(!token){
+        return res.status(401).json({message: "Access Denied! No Token Provided"})
+    }
+
+    try{
+        const decode = jwt.verify(token,process.env.JWT_SECRET)
+        req.user = decode
+        next()
+    }catch(error){
+        req.status(400).json({message: "Invalid Token"})
+    }
+}
+
+export default auth
