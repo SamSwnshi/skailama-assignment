a893d52fe11bea166f41cd12eb609b31c9f376af
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-04-26 12:08:49 +0530

    user schema created

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,20 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const auth = async(req,res,next)=>{
-    const token = req.header("Authorization")?.split(" ")
-    if(!token){
-        return res.status(401).json({message: "Access Denied! No Token Provided"})
-    }
+const auth = async (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ");
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access Denied! No Token Provided" });
+  }
 
-    try{
-        const decode = jwt.verify(token,process.env.JWT_SECRET)
-        req.user = decode
-        next()
-    }catch(error){
-        req.status(400).json({message: "Invalid Token"})
-    }
-}
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decode;
+    next();
+  } catch (error) {
+    req.status(400).json({ message: "Invalid Token" });
+  }
+};
 
-export default auth
+export default auth;
